8fbf8fc8cf38e8012ce57cf904d7969eca76b1b2
Yuni Nur Rohmatilah <89186114+yuninurr@users.noreply.github.com>
2024-06-12 01:53:21 +0700

    Add files via upload

--- main.go
+++ main.go
@@ -1,23 +1,68 @@
 package main
 
 import (
-	auth "api_gateway/usecase"
-	"fmt"
+	"api_gateaway/handler"
+	"api_gateaway/proto"
+	"context"
+	"net/http"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"github.com/go-micro/plugins/v4/client/grpc"
+	micro "go-micro.dev/v4"
+	"go-micro.dev/v4/client"
 )
 
 func main() {
-	
-	login := auth.NewLogin()
+	r := gin.Default()
 
-	username := "admin"
-	password := "admin123"
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:  []string{"*"},
+		AllowMethods:  []string{"*"},
+		AllowHeaders:  []string{"*"},
+		ExposeHeaders: []string{"*"},
+	}))
 
-	isAuthenticated := login.Authentication(username, password)
+	addrServiceTransactionOpt := client.WithAddress(":8084")
+	clientSrvTransaction := grpc.NewClient()
 
-	if isAuthenticated {
-		fmt.Println("Login successful")
-	} else {
-		fmt.Println("Invalid username or password")
-	}
+	srvTransaction := micro.NewService(
+		micro.Client(clientSrvTransaction),
+	)
+
+	srvTransaction.Init(
+		micro.Name("service-transaction"),
+		micro.Version("latest"),
+	)
+
+	accountRoute := r.Group("/account")
+	accountRoute.GET("/get", handler.NewAccount().GetAccount)
+	accountRoute.POST("/create", handler.NewAccount().CreateAccount)
+	accountRoute.PATCH("/update/:id", handler.NewAccount().UpdateAccount)
+	accountRoute.POST("/balance/", handler.NewAccount().BalanceAccount)
+	accountRoute.DELETE("/delete/:id", handler.NewAccount().DeleteAccount)
+
+	transactionRoute := r.Group("/transaction")
+	transactionRoute.GET("/get", func(g *gin.Context) {
+		clientResponse, err := proto.NewServiceTransactionService("service-transaction", srvTransaction.Client()).
+			Login(context.Background(), &proto.LoginRequest{
+				Username: "admin",
+				Password: "password123",
+			}, addrServiceTransactionOpt)
+
+		if err != nil {
+			g.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+		}
+
+		g.JSON(http.StatusOK, gin.H{
+			"data": clientResponse,
+		})
+	})
+	transactionRoute.POST("/transfer-bank", handler.NewTransaction().CreateTransaction)
+
+	authRoute := r.Group("/auth")
+	authRoute.POST("/login", handler.NewAuthRepository().Login)
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
-
